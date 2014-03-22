fbb36a783e37bcacafbc61787964ac8a82d2aabc
Anders Kirk Uhrenholt <akuhren@gmail.com>
2014-03-22 10:12:05 +0100

    .,

---
+++ www/js/preload.js
@@ -0,0 +1,27 @@
+function preload() {
+
+	var image0 = new Image();
+	image0.src="../icon.png";
+
+	var image1 = new Image();
+	image1.src="../images/1Borneteaterlejr.jpg";
+
+	var image2 = new Image();
+	image2.src="../images/BoerneTeaterLejr-2013--2048px.jpg";
+
+	var image3 = new Image();
+	image3.src="../images/BoerneTeaterLejr-i-Slangerup--2013-6898.jpg";
+
+	var image4 = new Image();
+	image4.src="../images/BoerneTeaterLejr-i-Slangerup--2013-7170.jpg";
+
+	var image5 = new Image();
+	image5.src="../images/BoerneTeaterLejr-i-Slangerup--2013-7362.jpg";
+
+	var image6 = new Image();
+	image6.src="../images/glyphicons_197_remove.png";
+
+	var image7 = new Image();
+	image7.src="../img/logo.png";
+
+}
